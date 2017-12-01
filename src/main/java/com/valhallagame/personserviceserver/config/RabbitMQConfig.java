c9af5617cc70ab5e8efe99cee0896e40e88383d2
Mikael Berglund <mikbe@kth.se>
2017-12-01 22:45:56 +0100

    Fixed serialization.

--- src/main/java/com/valhallagame/personserviceserver/config/RabbitMQConfig.java
+++ src/main/java/com/valhallagame/personserviceserver/config/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.valhallagame.personserviceserver.config;
 
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,18 @@ public class RabbitMQConfig {
 	@Bean
 	public DirectExchange personExchange() {
 		return new DirectExchange(RabbitMQRouting.Exchange.PERSON.name());
+	}
+	
+	@Bean
+	public Jackson2JsonMessageConverter jacksonConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+	
+	@Bean
+	public SimpleRabbitListenerContainerFactory ContainerFactory() {
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setMessageConverter(jacksonConverter());
+		return factory;
 	}
 
 }
