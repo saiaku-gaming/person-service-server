1514cb4fc5f51492783830de4a1ac21926ee7063
Mikael Berglund <mikbe@kth.se>
2018-01-03 09:37:24 +0100

    Removed logback localy, logback file now exists on live server.

--- src/main/java/com/valhallagame/personserviceserver/App.java
+++ src/main/java/com/valhallagame/personserviceserver/App.java
@@ -1,5 +1,11 @@
 package com.valhallagame.personserviceserver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
@@ -10,7 +16,18 @@ import com.valhallagame.common.DefaultServicePortMappings;
 @SpringBootApplication
 public class App {
 
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args) {
+		if (args.length > 0) {
+			// override system properties with local properties
+			try (InputStream inputStream = new FileInputStream(args[0])) {
+				System.getProperties().load(inputStream);
+			} catch (IOException e) {
+				logger.error("Failed to read input.", e);
+			}
+		}
+
 		SpringApplication.run(App.class, args);
 	}
 
