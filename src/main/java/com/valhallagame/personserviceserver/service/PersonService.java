3ddd9c1a048db20e0e6a1f88ae2a39c6b1b95e32
phrozen <phrozen@teamsamst.com>
2019-03-15 22:45:48 +0100

    Set persons offline if we did not have a heartbeat in 120 seconds

--- src/main/java/com/valhallagame/personserviceserver/service/PersonService.java
+++ src/main/java/com/valhallagame/personserviceserver/service/PersonService.java
@@ -110,7 +110,8 @@ public class PersonService {
 
 		Response response = client.newCall(request).execute();
 
-		String jsonBody = response.body().string();
+        assert response.body() != null;
+        String jsonBody = response.body().string();
 
 		ObjectMapper mapper = new ObjectMapper();
 
@@ -145,4 +146,23 @@ public class PersonService {
 			debugPersons.remove(key);
 		}
 	}
+
+    public void markPersonsOffline() {
+        getOnlinePersons()
+                .stream()
+                .filter(person -> person.getLastHeartbeat().isBefore(Instant.now().minusSeconds(120)))
+                .forEach(this::setPersonOffline);
+    }
+
+    public void setPersonOffline(Person user) {
+        if (user.isOnline()) {
+            user.setOnline(false);
+            user = savePerson(user);
+            rabbitSender.sendMessage(
+                    RabbitMQRouting.Exchange.PERSON,
+                    RabbitMQRouting.Person.OFFLINE.name(),
+                    new NotificationMessage(user.getUsername(), "Offline")
+            );
+        }
+    }
 }
